ae2556988639b013a4ca070327487eb6312679ae
banditejaswani-dot <bandi.tejaswani@capgemini.com>
2025-09-19 15:20:51 +0530

    Add files via upload

---
+++ pythonsample.py
@@ -0,0 +1,11 @@
+# Define a function to calculate the area of a rectangle
+def calculate_area(length, width):
+
+    area = length * width
+
+    return area
+# Example usage
+length = 10
+width = 5
+result = calculate_area(length, width)
+print("The area of the rectangle is:", result)
